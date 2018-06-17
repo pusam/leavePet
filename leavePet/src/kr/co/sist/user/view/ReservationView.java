793346fc5640a1149ff8a6efd703f4d2691133a4
kdh <kdh@kdh-PC>
2018-06-17 23:04:51 +0900

    dd

--- leavePet/src/kr/co/sist/user/view/ReservationView.java
+++ leavePet/src/kr/co/sist/user/view/ReservationView.java
@@ -1,10 +1,22 @@
 package kr.co.sist.user.view;
 
-public class ReservationView {
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
+/**
+ * 사용자 견 예약신청 화면
+ * @author kdh
+ */
+public class ReservationView extends JFrame{
+
+	private JLabel lblReservation, lblSelectDate, lblKind, lblGuardian, lblGuardianPhone, lblPreliminaryPhone;
+	
+	public ReservationView() {
+		
+	}//ReservationView
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		new ReservationView();
+	}//end main
 
-	}
-
-}
+}//class
